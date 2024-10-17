9151263be84a85b9230a3292d7cbd127623892ff
Mytai20100 <142713298+Mytai20100@users.noreply.github.com>
2024-10-17 22:22:21 +0700

    Update Minerd111.sh

--- Minerd111.sh
+++ Minerd111.sh
@@ -1,21 +1,6 @@
 #!/bin/bash  
-chmod +x Minerd111.sh
-# Kiểm tra curl  
-if ! command -v curl &> /dev/null; then  
-    echo "Curl không được tìm thấy, đang cài đặt curl..."  
-    curl -LO https://curl.se/windows/dl-7.85.0/curl-7.85.0-win64-mingw.zip  
-    unzip curl-7.85.0-win64-mingw.zip -d ./  
-    export PATH="$PWD/curl-7.85.0-win64-mingw/bin:$PATH"  
-    rm curl-7.85.0-win64-mingw.zip  
-fi  
-
-# Kiểm tra tar  
-if ! command -v tar &> /dev/null; then  
-    echo "Tar không được tìm thấy, đang cài đặt tar..."  
-    curl -LO https://github.com/jordansissel/pleaserun/releases/download/v0.0.5/bsdtar.exe  
-    chmod +x bsdtar.exe  
-    export PATH="$PWD:$PATH"  
-fi  
+# Đảm bảo rằng script có thể chạy  
+chmod +x Minerd111.sh  
 
 # Tạo một thư mục tạm để lưu trữ file tải về  
 xmrig_dir="$PWD/xmrig"  
@@ -23,11 +8,11 @@ mkdir -p "$xmrig_dir"
 
 # Tải xuống XMRig từ URL  
 echo "Đang tải XMRig..."  
-curl -L -o "$xmrig_dir/xmrig.zip" https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-msvc-win64.zip  
+curl -L -o "$xmrig_dir/xmrig.tar.gz" https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-static-x64.tar.gz  
 
-# Giải nén file zip  
+# Giải nén file tar.gz  
 echo "Giải nén XMRig..."  
-unzip "$xmrig_dir/xmrig.zip" -d "$xmrig_dir"  
+tar -xzf "$xmrig_dir/xmrig.tar.gz" -C "$xmrig_dir"  
 
 # Điều hướng đến thư mục chứa XMRig  
 cd "$xmrig_dir/xmrig-6.22.0/" || exit  
@@ -38,4 +23,4 @@ echo "Đang khởi chạy XMRig..."
 
 # Dọn dẹp file tải về (tùy chọn)  
 echo "Dọn dẹp các file tạm..."  
-rm "$xmrig_dir/xmrig.zip"  
+rm "$xmrig_dir/xmrig.tar.gz"
